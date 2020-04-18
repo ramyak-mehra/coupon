bfba754db62539b3864d88603878932fc92a258f
ramyak-mehra <rmehra_be19@thapar.edu>
2020-04-18 22:22:19 +0530

    lolddd

--- coupan_site/settings.py
+++ coupan_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import djago_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'coupan-home'
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
