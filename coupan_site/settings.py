ab9c9454920c6a667ac968612553c42fd228abe5
ramyak-mehra <rmehra_be19@thapar.edu>
2020-04-18 22:04:51 +0530

    Secondd

--- coupan_site/settings.py
+++ coupan_site/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&1-pnp)o=qtj*2o3skz(p7y#_l61zn+rp$20_(-#sq(i-n_1nc'
-
+#SECRET_KEY = '&1-pnp)o=qtj*2o3skz(p7y#_l61zn+rp$20_(-#sq(i-n_1nc'
+SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mycouponapp.herokuapp.com/']
 
 
 # Application definition
